fdf8e4ef161f1d9948c19f623c676978cd037753
Fábio Luiz Nery de Miranda <fabio@miranti.net.br>
2012-12-22 04:36:25 -0200

    Enhances (generalizes) xml test code

--- test/spree_cielo/base_test.rb
+++ test/spree_cielo/base_test.rb
@@ -1,12 +1,28 @@
 describe SpreeCielo::Base do
   subject { SpreeCielo::Base.new }
 
+  let(:id)          { "1" }
+  let(:versao)      { "1.2.0" }
+  let(:xml_header)  { '<?xml version="1.0" encoding="UTF-8"?>' }
+
+  before do
+    subject.id = id
+    subject.versao = versao
+  end
+
+  def expected_xml opts={}
+    opts.reverse_merge! root: "base", id: id, versao: versao
+    root, id, versao = opts[:root], opts[:id], opts[:versao]
+
+    res = xml_header
+    unless block_given?
+      res << %|<#{root} id="#{id}" versao="#{versao}"/>|
+    else
+      res << %|<#{root} id="#{id}" versao="#{versao}">#{yield}</#{root}>|
+    end
+  end
+
   it "serializes" do
-    subject.id = 1
-    subject.versao = "1.2.0"
-    expected_xml =
-      %|<?xml version="1.0" encoding="UTF-8"?>| +
-      %|<base id="1" versao="1.2.0"/>|
     assert_equal expected_xml, subject.to_xml
   end
 end
