9008dc7a6a4f8c0d7ef0d2e96b3441bf85381300
Fábio Luiz Nery de Miranda <fabio@miranti.net.br>
2012-12-22 11:35:14 -0200

    Serializes Base attributes

--- lib/spree_cielo/base.rb
+++ lib/spree_cielo/base.rb
@@ -2,12 +2,28 @@ require 'active_support/core_ext/string'
 require 'builder'
 
 class SpreeCielo::Base
-  attr_accessor :id, :versao
+  attr_accessor :id, :versao, :dados_ec
 
   def to_xml
     x = Builder::XmlMarkup.new
     x.instruct!
     name = self.class.name.demodulize
-    x.tag! name.underscore.dasherize, id: id, versao: versao
+    x.tag! name.underscore.dasherize, id: id, versao: versao do
+      (instance_variables - [:@id, :@versao]).each { |attr|
+        x.tag! dash(attr) do
+          value = instance_variable_get attr
+
+          value.instance_variables.each do |vattr|
+            attr_value = value.instance_variable_get "@#{vattr}"
+            x.tag! dash(vattr), attr_value
+          end
+        end
+      }
+    end
+  end
+
+  private
+  def dash value
+    value.to_s.gsub("@", "").dasherize
   end
 end
--- test/spree_cielo/base_test.rb
+++ test/spree_cielo/base_test.rb
@@ -15,14 +15,33 @@ describe SpreeCielo::Base do
     root, id, versao = opts[:root], opts[:id], opts[:versao]
 
     res = xml_header
-    unless block_given?
-      res << %|<#{root} id="#{id}" versao="#{versao}"/>|
-    else
-      res << %|<#{root} id="#{id}" versao="#{versao}">#{yield}</#{root}>|
-    end
+    res << %|<#{root} id="#{id}" versao="#{versao}">| +
+            "#{yield if block_given?}</#{root}>"
   end
 
   it "serializes" do
     assert_equal expected_xml, subject.to_xml
+  end
+
+  let(:ec) { MiniTest::Mock.new }
+
+  it "seralizes attributes" do
+    numero, chave = 123, "M3str4"
+    attributes = { numero: numero, chave: chave }
+
+    ec.expect :instance_variables, attributes.keys
+    attributes.each { |k,v|
+      ec.expect :instance_variable_get, v, ["@#{k}"]
+    }
+
+    subject.dados_ec = ec
+
+    xml = expected_xml {
+      "<dados-ec>" +
+        "<numero>#{numero}</numero>" +
+        "<chave>#{chave}</chave>" +
+      "</dados-ec>"
+    }
+    assert_equal xml, subject.to_xml
   end
 end
