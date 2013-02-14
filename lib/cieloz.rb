6eb403dd9fed255ef47a4f85206a2e1a4403fa84
Fábio Luiz Nery de Miranda <fabio@miranti.net.br>
2013-02-14 04:18:32 -0200

    Moves Cieloz::Helper to a file

--- lib/cieloz.rb
+++ lib/cieloz.rb
@@ -7,37 +7,9 @@ require 'nokogiri'
 DIR = File.dirname __FILE__
 I18n.load_path += Dir.glob "#{DIR}/../config/locales/*.{rb,yml}"
 
+require "cieloz/helpers"
+
 module Cieloz
-  module Helpers
-    module ClassMethods
-      def hattr_writer *attrs
-        attrs.each { |attr|
-          define_method "#{attr}=" do |value|
-            value = eval(attr.to_s.constantize).new(value) if value.is_a? Hash
-            instance_variable_set "@#{attr}", value
-            yield(value) if block_given?
-          end
-        }
-      end
-    end
-
-    def self.included base
-      base.extend ClassMethods
-      base.send :include, ActiveModel::Validations
-    end
-
-    def initialize attrs={}
-      self.attributes = attrs
-    end
-
-    def attributes= attrs
-      attrs.each {|k,v|
-        m = "#{k}="
-        send(m, v) if respond_to? m
-      }
-    end
-  end
-
   class DadosEc
     include Helpers
 
---
+++ lib/cieloz/helpers.rb
@@ -0,0 +1,31 @@
+module Cieloz
+  module Helpers
+    module ClassMethods
+      def hattr_writer *attrs
+        attrs.each { |attr|
+          define_method "#{attr}=" do |value|
+            value = eval(attr.to_s.constantize).new(value) if value.is_a? Hash
+            instance_variable_set "@#{attr}", value
+            yield(value) if block_given?
+          end
+        }
+      end
+    end
+
+    def self.included base
+      base.extend ClassMethods
+      base.send :include, ActiveModel::Validations
+    end
+
+    def initialize attrs={}
+      self.attributes = attrs
+    end
+
+    def attributes= attrs
+      attrs.each {|k,v|
+        m = "#{k}="
+        send(m, v) if respond_to? m
+      }
+    end
+  end
+end
