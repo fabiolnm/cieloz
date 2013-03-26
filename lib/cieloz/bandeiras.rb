f6a0668672b6b8be5524b1ae4cdb6b387cc77c6a
Fábio Luiz Nery de Miranda <fabio@miranti.net.br>
2013-03-26 11:37:50 -0300

    Makes Bandeiras#operacao resilient to nil errors

--- lib/cieloz/bandeiras.rb
+++ lib/cieloz/bandeiras.rb
@@ -4,13 +4,13 @@ module Cieloz
     AMEX, DINERS, DISCOVER, ELO, MASTERCARD, VISA = ALL
 
     def self.operacao produto
-      case produto.to_sym
-      when :mastercard_securecode
+      produto = produto.to_s
+      case produto
+      when 'mastercard_securecode'
         [MASTERCARD,      :autorizar_somente_autenticada]
-      when :verified_by_visa
+      when 'verified_by_visa'
         [VISA,            :autorizar_somente_autenticada]
       else
-        produto = produto.to_s
         if ALL.include? produto
           [produto,  :autorizacao_direta]
         else
